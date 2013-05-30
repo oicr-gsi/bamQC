a3c0cdab2e682570e38141dfc07e3487df18e167
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2013-05-30 16:04:37 -0400

    BamQC java workflow provisioned output file fix; BamQC/FastQC code cleanup
    Former-commit-id: 966c35e463df5f84f35d19f4198dcd9b8217e57e

--- java/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
+++ java/src/main/java/ca/on/oicr/pde/workflows/WorkflowClient.java
@@ -4,28 +4,49 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.sourceforge.seqware.pipeline.workflowV2.AbstractWorkflowDataModel;
+import net.sourceforge.seqware.pipeline.workflowV2.model.Command;
 import net.sourceforge.seqware.pipeline.workflowV2.model.Job;
 import net.sourceforge.seqware.pipeline.workflowV2.model.SqwFile;
 
 public class WorkflowClient extends AbstractWorkflowDataModel {
 
     String outdir = null;
-    
-    @Override
-    public Map<String, SqwFile> setupFiles() {
+    private static final Logger logger = Logger.getLogger(WorkflowClient.class.getName());
+    //workflow parameters
+    private String queue = null;
+    private String inputFile = null;
+    private String outputPrefix = null;
+    private String outputDir = null;
+    //private String jsonOutputFile = null;
+    private String sampleRate = null;
+    private String normalInsertMax = null;
+    private String mapQualCut = null;
+    private String targetBed = null;
+    private String jsonMetadataFile = null;
+    //workflow directories
+    private String binDir = null;
+    private String dataDir = null;
+    private String finalOutputDir = null;
+
+    //Constructor - called in setupDirectory()
+    private void WorkflowClient() {
+
+        binDir = getWorkflowBaseDir() + "/bin/";
+        dataDir = "data/";
 
         try {
-            // register an input file
-            SqwFile file0 = this.createFile("file_in_0");
-            file0.setSourcePath(getProperty("input_file"));
-            file0.setType("text/plain");
-            file0.setIsInput(true);
-
-            return this.getFiles();
+            queue = getProperty("queue");
+            inputFile = getProperty("input_file");
+            outputDir = getProperty("output_dir");
+            outputPrefix = getProperty("output_prefix");
+            finalOutputDir = outputPrefix + outputDir + "/seqware-" + getSeqware_version() + "_" + getName() + "_" + getVersion() + "/" + getRandom() + "/";
+            sampleRate = getProperty("sample_rate");
+            normalInsertMax = getProperty("normal_insert_max");
+            mapQualCut = getProperty("map_qual_cut");
+            targetBed = getProperty("target_bed");
+            jsonMetadataFile = getProperty("json_metadata_file");
         } catch (Exception ex) {
-            ex.printStackTrace();
-            Logger.getLogger(WorkflowClient.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+            logger.log(Level.SEVERE, "Expected parameter missing", ex);
         }
 
     }
@@ -33,103 +54,69 @@ public class WorkflowClient extends AbstractWorkflowDataModel {
     @Override
     public void setupDirectory() {
 
-//        //String outdir = null;
-//        try {
-//            String outputPrefix = getProperty("output_prefix");
-//            String outputDir = getProperty("output_dir");
-//            if (outputPrefix != null && outputDir != null) {
-//                outdir = outputPrefix + outputDir;
-//                outdir = outdir.endsWith("/") ? outdir : outdir + "/";
-//                this.addDirectory(outdir);
-//            } else {
-//                String inputPath = this.getFiles().get("file_in_0").getProvisionedPath();
-//                outdir = inputPath.substring(0, outdir.lastIndexOf("/"));
-//            }
-//
-//        } catch (Exception ex) {
-//            Logger.getLogger(WorkflowClient.class.getName()).log(Level.WARNING, null, ex);
-//        }
+        WorkflowClient(); //Constructor call
+        addDirectory(dataDir);
+        addDirectory(finalOutputDir);
+
+    }
+
+    @Override
+    public Map<String, SqwFile> setupFiles() {
+
+            SqwFile file0 = this.createFile("file_in_0");
+            file0.setSourcePath(inputFile);
+            file0.setType("application/bam");
+            file0.setIsInput(true);
+
+            return this.getFiles();
+
     }
 
     @Override
     public void buildWorkflow() {
 
-        // Workflow parameters
-        String queue = null;
-        String outputPath = null;
-
-        // load Workflow parameters
-        try {
-            queue = getProperty("queue");
-            if (queue == null) {
-                queue = "default";
-            }
-        } catch (Exception ex) {
-            queue = "default";
-            Logger.getLogger(WorkflowClient.class.getName()).log(Level.WARNING, "queue not defined, using default queue");
-        }
-        
-//        try {
-//            outputPath = getProperty("output_path");
-//
-//            if (outputPath.equals("NA")) {
-//                // set output path to same as input
-//                this.getFiles().get("file_in_0").getProvisionedPath();
-//            }
-//            outputPath = outputPath.equalsIgnoreCase("NA") ? outputPath : this.getFiles().get("file_in_0").getProvisionedPath();
-//        } catch (Exception ex){
-//            
-//        }
-
-        // bamToJsonStats parameters
-        String inputFile = null;
-        String jsonOutputFile = null;
-        String sampleRate = null;
-        String normalInsertMax = null;
-        String mapQualCut = null;
-        String targetBed = null;
-        String jsonMetadataFile = null;
-        boolean bamToJsonStatsIsReady = false;
-
-        // load bamToJsonStats parameters
-        //TODO: Sanitize input
-        try {
-            inputFile = this.getFiles().get("file_in_0").getProvisionedPath();
-            jsonOutputFile = inputFile + ".BamQC.json";
-            sampleRate = getProperty("sample_rate");
-            normalInsertMax = getProperty("normal_insert_max");
-            mapQualCut = getProperty("map_qual_cut");
-            targetBed = getProperty("target_bed");
-            jsonMetadataFile = getProperty("json_metadata_file");
-            bamToJsonStatsIsReady = true;
-        } catch (Exception ex) {
-            Logger.getLogger(WorkflowClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        if (bamToJsonStatsIsReady) {
-            Job job00 = this.getWorkflow().createBashJob("bamToJsonStats");
-            job00.setCommand(
-                    getWorkflowBaseDir() + "/bin" + "/samtools-0.1.19/samtools " + "view " + inputFile
-                    + " | "
-                    + getWorkflowBaseDir() + "/bin" + "/perl-5.14.1/perl "
-                    + getWorkflowBaseDir() + "/bin" + "/samStats.pl "
-                    + "-s " + sampleRate + " "
-                    + "-i " + normalInsertMax + " "
-                    + "-q " + mapQualCut + " "
-                    + "-r " + targetBed + " "
-                    + "-j " + jsonMetadataFile
-                    + " > " + jsonOutputFile);
+            Job job00 = getBamQcJob();
             job00.setMaxMemory("2000");
             job00.setQueue(queue);
 
-            SqwFile file1 = new SqwFile();
-            file1.setSourcePath(jsonOutputFile);
-            file1.setType("text/json");
-            file1.setIsOutput(true);
-            file1.setForceCopy(true);
-        } else {
-            Logger.getLogger(WorkflowClient.class.getName()).log(Level.SEVERE, "bamToJsonStats is missing required parameters");
-        }
+    }
+    
+    private Job getBamQcJob(){
+        
+        Job job = getWorkflow().createBashJob("BamToJsonStats");
+        
+        String jsonOutputFileName = inputFile.substring(inputFile.lastIndexOf("/") + 1) + ".BamQC.json";
+        
+        Command command = job.getCommand();
+        command.addArgument(getWorkflowBaseDir() + "/bin" + "/samtools-0.1.19/samtools " + "view " + inputFile);
+        command.addArgument("|"); //pipe to
+        command.addArgument(getWorkflowBaseDir() + "/bin" + "/perl-5.14.1/perl");
+        command.addArgument(getWorkflowBaseDir() + "/bin" + "/samStats.pl");
+        command.addArgument("-s " + sampleRate);
+        command.addArgument("-i " + normalInsertMax);
+        command.addArgument("-q " + mapQualCut);
+        command.addArgument("-r " + targetBed);
+        command.addArgument("-j " + jsonMetadataFile);
+        command.addArgument(">"); //redirect to
+        command.addArgument(dataDir + jsonOutputFileName);
+        
+        SqwFile sqwJsonOutputFile = createOutFile(dataDir + jsonOutputFileName, "text/json", finalOutputDir + jsonOutputFileName, true);
+        job.addFile(sqwJsonOutputFile);
+        
+        return job;
+        
+    }
+
+    private SqwFile createOutFile(String sourcePath, String sourceType, String outputPath, boolean forceCopy) {
+
+        SqwFile file = new SqwFile();
+        file.setSourcePath(sourcePath);
+        file.setType(sourceType);
+        file.setIsOutput(true);
+        file.setOutputPath(outputPath);
+        file.setForceCopy(forceCopy);
+
+        return file;
 
     }
 }
