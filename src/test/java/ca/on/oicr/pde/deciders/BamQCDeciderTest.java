75f38f0dbf16c8334e1b3a9c88d16ca7e53f7703
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-02-07 14:41:57 -0500

    Update decider-bam-qc to use TestNG rather than JUnit
    Former-commit-id: c84aaa0000f8e2ce9a0b41c41dc9b0c73000e36d

--- src/test/java/ca/on/oicr/pde/deciders/BamQCDeciderTest.java
+++ src/test/java/ca/on/oicr/pde/deciders/BamQCDeciderTest.java
@@ -7,17 +7,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import net.sourceforge.seqware.common.module.FileMetadata;
 import net.sourceforge.seqware.common.module.ReturnValue;
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 import org.xml.sax.SAXException;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 /**
  *
@@ -26,62 +18,43 @@ import org.xml.sax.SAXException;
 public class BamQCDeciderTest {
 
     File filepath = FileUtils.toFile(this.getClass().getResource("/rsconfig.xml"));
-
+   
     public BamQCDeciderTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
     }
 
     /**
      * Test of init method, of class BamQCDecider.
      */
-    @Test
-    @Ignore
+    @Test(enabled = false)
     public void testInit() {
         System.out.println("init");
         BamQCDecider instance = new BamQCDecider();
         ReturnValue expResult = null;
         ReturnValue result = instance.init();
-        assertEquals(expResult, result);
+        Assert.assertEquals(result, expResult);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.fail("The test case is a prototype.");
     }
 
     /**
      * Test of handleGroupByAttribute method, of class BamQCDecider.
      */
-    @Test
-    @Ignore
+    @Test(enabled = false)
     public void testHandleGroupByAttribute() {
         System.out.println("handleGroupByAttribute");
         String attribute = "";
         BamQCDecider instance = new BamQCDecider();
         String expResult = "";
         String result = instance.handleGroupByAttribute(attribute);
-        assertEquals(expResult, result);
+        Assert.assertEquals(result, expResult);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.fail("The test case is a prototype.");
     }
 
     /**
      * Test of checkFileDetails method, of class BamQCDecider.
      */
-    @Test
-    @Ignore
+    @Test(enabled = false)
     public void testCheckFileDetails() {
         System.out.println("checkFileDetails");
         ReturnValue returnValue = null;
@@ -89,16 +62,15 @@ public class BamQCDeciderTest {
         BamQCDecider instance = new BamQCDecider();
         boolean expResult = false;
         boolean result = instance.checkFileDetails(returnValue, fm);
-        assertEquals(expResult, result);
+        Assert.assertEquals(result, expResult);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.fail("The test case is a prototype.");
     }
 
     /**
      * Test of modifyIniFile method, of class BamQCDecider.
      */
-    @Test
-    @Ignore
+    @Test(enabled = false)
     public void testModifyIniFile() {
         System.out.println("modifyIniFile");
         String commaSeparatedFilePaths = "";
@@ -106,26 +78,21 @@ public class BamQCDeciderTest {
         BamQCDecider instance = new BamQCDecider();
         Map expResult = null;
         Map result = instance.modifyIniFile(commaSeparatedFilePaths, commaSeparatedParentAccessions);
-        assertEquals(expResult, result);
+        Assert.assertEquals(result, expResult);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Assert.fail("The test case is a prototype.");
     }
-    
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
-    @Test
+    @Test(expectedExceptions = Exception.class)
     public void configFromParsedXML_missingXMLFile() throws ParserConfigurationException, SAXException, IOException, Exception {
 
-        exception.expect(Exception.class);
         BamQCDecider.configFromParsedXML("doesNotExist.xml", "");
 
     }
 
-    @Test
+    @Test(expectedExceptions = Exception.class)
     public void configFromParsedXML_missingResequencingTag() throws ParserConfigurationException, SAXException, IOException, Exception {
 
-        exception.expect(Exception.class);
         BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/missingTargetResequencingTag.xml")).getPath(), "");
 
     }
@@ -133,25 +100,24 @@ public class BamQCDeciderTest {
     @Test
     public void configFromParsedXML_getAllIntervalFilePaths() throws IOException, ParserConfigurationException, SAXException, Exception {
 
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/HALT/halt_coding.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "HALT"));
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/Illumina.TruSeq/TruSeq-Exome-Targeted-Regions-BED-file.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Illumina TruSeq Exome"));
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/Agilent.SureSelect.ICGC/sanger.exons.hg19.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Agilent SureSelect ICGC/Sanger Exon"));
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/Agilent.SureSelect.G3362/SureSelect_All_Exon_G3362_with_names.v2.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Agilent SureSelect All Exon G3362"));
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/Nimblegen.2.1M.Human.Exome/2.1M_Human_Exome.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Nimblegen 2.1M Human Exome (21191)"));
-        Assert.assertEquals("/.mounts/labs/PDE/data/TargetedSequencingQC/TruSeqCancerPanel/TruSeq_Cancer_Panel_Targets_Regions_sorted.bed",
-                BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "TruSeq Amplicon - Cancer Panel"));
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "HALT"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/HALT/halt_coding.bed");
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Illumina TruSeq Exome"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/Illumina.TruSeq/TruSeq-Exome-Targeted-Regions-BED-file.bed");
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Agilent SureSelect ICGC/Sanger Exon"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/Agilent.SureSelect.ICGC/sanger.exons.hg19.bed");
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Agilent SureSelect All Exon G3362"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/Agilent.SureSelect.G3362/SureSelect_All_Exon_G3362_with_names.v2.bed");
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "Nimblegen 2.1M Human Exome (21191)"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/Nimblegen.2.1M.Human.Exome/2.1M_Human_Exome.bed");
+        Assert.assertEquals(BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig.xml")).getPath(), "TruSeq Amplicon - Cancer Panel"), 
+                "/.mounts/labs/PDE/data/TargetedSequencingQC/TruSeqCancerPanel/TruSeq_Cancer_Panel_Targets_Regions_sorted.bed");
 
     }
 
-    @Test
+    @Test(expectedExceptions = Exception.class)
     public void configFromParsedXML_duplicateTargetSequencingCheck() throws ParserConfigurationException, SAXException, IOException, Exception {
 
-        exception.expect(Exception.class);
         BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/rsconfig-duplicates-bottom.xml")).getPath(), "");
 
     }
@@ -163,10 +129,9 @@ public class BamQCDeciderTest {
 
     }
     
-    @Test
+    @Test(expectedExceptions = Exception.class)
     public void configFromParsedXML_noneXmlFile() throws ParserConfigurationException, SAXException, IOException, Exception {
-        
-        exception.expect(SAXException.class);
+
         BamQCDecider.configFromParsedXML(FileUtils.toFile(BamQCDeciderTest.class.getResource("/notAnXmlFile.txt")).getPath(), "");
         
     }
