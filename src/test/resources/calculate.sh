1f7976d2f8a5e843a10c138dfbb68889c016f9b9
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2015-01-07 17:02:26 -0500

    Modified the calculate script to print out the JSON and the result of the jsonToGenericRunReport.pl scripts to the metrics files. Also modified the bamqc scripts to point to the spb directory. Added bamqc as dependency in POM, included install script, modified calculate script to use the script in the bundle

--- src/test/resources/calculate.sh
+++ src/test/resources/calculate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
 
 cd $1
-find . -type f -exec md5sum {} +
+find . -type f -exec python -mjson.tool {} +
+find . -type f -exec jsonToGenericRunReport.pl {} + | grep -v "Generic run report generated on"
