26c78be06079565ee75904d7b58e372d44353324
iainrb <1695061+iainrb@users.noreply.github.com>
2021-06-01 17:40:12 -0400

    Add comment

--- calculate.sh
+++ calculate.sh
@@ -6,6 +6,7 @@ set -o pipefail
 cd $1
 
 module load jq
+# attempt to load different module versions in case of conflict
 module load python/3.9 || module load python/3.7 || module load python/3.6
 # remove the Picard header because it includes temporary paths
 find . -xtype f -exec jq 'del(.picard | .header)' {} \; | python3 -mjson.tool --sort-keys
